6f848fcb55a32e433ae9ee40611bf0542df032e2
Suyash-Singh5 <suyash.s.singh5@gmail.com>
2021-07-05 17:28:23 +0530

    Ready to Deploy on Heroku

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 require("dotenv").config();
+const path = require("path")
 const express = require("express");
 const http = require("http");
 const { Socket } = require("net");
@@ -128,5 +129,13 @@ io.on('connection', socket => {
     });
 
 });
+
+if(process.env.PROD){
+    app.use(express.static(path.join(__dirname,"./client/build")));
+    app.get('*',(req,res)=>{
+        res.sendFile(path.join(__dirname,'./client/build/index.html'));
+    });
+}
+
 const port = process.env.PORT || 3030
 server.listen(port,()=>{console.log(`server running at port ${port}`)});
